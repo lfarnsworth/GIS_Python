308411a9e10162bcc7a9e3a5fa11311c0ba3b15b
Lauren Farnsworth <78186575+lfarnsworth@users.noreply.github.com>
2021-02-16 21:46:00 -0500

    Create 5-Scrabble.py

---
+++ Coding_Challenges/Challenge2/5-Scrabble.py
@@ -0,0 +1,30 @@
+# Given Dictionary
+letter_scores = {
+    "aeioulnrst": 1,
+    "dg": 2,
+    "bcmp": 3,
+    "fhvwy": 4,
+    "k": 5,
+    "jx": 8,
+    "qz": 10
+}
+
+# Ask user to enter word
+word_str = input("Enter a word: ")
+
+# Start score at zero (initialize the variable)
+calculated_score = 0
+
+# Step through word string letter by letter
+for letter in word_str:
+    # get list of keys, search for letter later
+    list_of_keys = letter_scores.keys()
+    # step through each item in the list
+    for key_item in list_of_keys:
+        # check if the letter is in the current item
+        if letter in key_item:
+            # Find number associated with the key
+            this_letters_score = letter_scores.get(key_item)
+            # Add score to the running tally
+            calculated_score = calculated_score + this_letters_score
+print("Your score is: " + str(calculated_score))
