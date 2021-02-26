37d907a0b321181310464a411bf0dacdc6b6792d
Lauren Farnsworth <78186575+lfarnsworth@users.noreply.github.com>
2021-02-26 00:04:23 -0500

    Create push_limit.py

---
+++ Coding_Challenges/CodingChallenge3/push_limit.py
@@ -0,0 +1,13 @@
+# Bad Password Generator
+# Takes 3 arguments (Name, Street, Birth year) and generates awful password
+# Load library to get command-line arguments and select random value
+import sys
+import random
+
+street_name = sys.argv[1]
+birth_year = sys.argv[2]
+first_name = sys.argv[3]
+
+special_char = random.choice('`~!@#$%^&*()_+')
+print('This is your terrible password: ')
+print(street_name[:3] + birth_year[-2:] + first_name[::-1] + special_char)
